2580d178c7471d8708bfedfb59e005ac06be3075
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-01-11 23:31:54 +0300

    react giris odev 1 eklendi

---
+++ react-giris-odev/app.js
@@ -0,0 +1,10 @@
+export default async function getData(userId) {
+  const { data: user } = await axios(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+  const { data: posts } = await axios(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+  );
+
+  return { ...user, posts };
+}
---
+++ react-giris-odev/index.js
@@ -0,0 +1,4 @@
+import getData from "./app.js";
+const result = await getData(2);
+
+console.log(result);
