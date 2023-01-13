ef29a586c69ed9570053c5c93d5f717e9f31a86a
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-01-13 22:40:47 +0300

    react-todo-odev eklendi

---
+++ react-todo-odev/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-titleColor text-center font-medium text-8xl">
+        todos
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-todo-odev/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        titleColor: "rgb(175, 47, 47, 0.15)",
+      },
+    },
+  },
+  plugins: [],
+};
