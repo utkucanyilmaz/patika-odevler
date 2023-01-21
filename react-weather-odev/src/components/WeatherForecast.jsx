002a3c70ba198ea68dfffae999a70f9381f6a11e
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-01-21 22:40:30 +0300

    api source changed

--- react-weather-odev/src/components/WeatherForecast.jsx
+++ react-weather-odev/src/components/WeatherForecast.jsx
@@ -4,10 +4,11 @@ import axios from "axios";
 
 function WeatherForecast() {
   const [weather, setWeather] = useState();
+
   const { city } = useWeather();
   useEffect(() => {
     axios(
-      `https://api.open-meteo.com/v1/forecast?latitude=${city.latitude}&longitude=${city.longitude}&timezone=Europe%2FMoscow&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      `https://api.weatherbit.io/v2.0/forecast/daily?key=892f31c5e9b84e69bb4a08e47db71c43&lat=${city.latitude}&lon=${city.longitude}&days=7`
     )
       .then(data => {
         console.log(data.data);
